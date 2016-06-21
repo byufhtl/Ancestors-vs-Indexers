309f4ee2a240ee9fb6cfc9163be319bfc351c780
Calvin McMurray <calvin.r.mcm@gmail.com>
2016-06-21 12:24:10 -0600

    Initial Code Commit. Includes the requirejs framework for the program.

---
+++ main.js
@@ -0,0 +1,25 @@
+/**
+ * Created by calvinmcm on 6/21/16.
+ */
+
+require.config({
+    baseUrl: 'src/js',
+    paths: {
+        familysearch: 'https://cdn.jsdelivr.net/familysearch-javascript-sdk/2.1.0/familysearch-javascript-sdk.min',
+        jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
+        bootstrap: 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min'
+    },
+    shim: {
+        familysearch:{
+            deps: ['jquery']
+        },
+        bootstrap: {
+            deps: ['jquery']
+        }
+    }
+});
+
+require(['jquery', 'familysearch', 'bootstrap'],function($, FamilySearch, Bootstrap){
+
+});
+

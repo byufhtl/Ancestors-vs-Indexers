2af133947e550532fc5a67c0f12a182b8e08a51d
Calvin McMurray <calvin.r.mcm@gmail.com>
2016-07-01 14:12:54 -0600

    Worked on the click and drag functionality and got it into rough working shape. Controls are 'sticky,' meaning that there is a buffer that builds around the mousedown point that is still considered a static click. Upon leaving the buffer, the canvas image will 'jump' to the mouse point and will enter click-and-drag mode, which replaces the click response with the click-and-drag response.:

--- src/js/CanvasManager.js
+++ src/js/CanvasManager.js
@@ -17,23 +17,41 @@ define(['jquery', 'GEvent', 'Point', 'ViewTransform'], function($, GEvent, Point
         var dragged = false;
 
         var start;
+        var origin;
+        var buffer;
 
         canvas.mousedown(function(event){
             draggable = true;
+            dragged = false;
 
             start = new Point(event.pageX, event.pageY);
+            origin = new Point(self.viewTransform.getX(), self.viewTransform.getY());
+            buffer = new Point(0, 0);
         });
 
         canvas.mousemove(function(event){
             if(draggable){
-                dragged = true;
                 var diff = new Point(event.pageX - start.X, event.pageY - start.Y);
                 start = new Point(event.pageX, event.pageY);
 
-                self.viewTransform.addX(diff.X);
-                self.viewTransform.addY(diff.Y);
+                if(buffer) { // If we still have a buffer, keep going
+                    buffer = new Point(buffer.X + diff.X, buffer.Y + diff.Y);
+                    console.log(buffer);
+                    // When we move out of the buffer, plug the buffer storage into the movement as well, then kill buffer.
+                    if(buffer.X > 15 || buffer.X < -15 || buffer.Y > 15 || buffer.Y < -15){
+                        //console.log("Killing buffer.");
+                        diff = new Point(buffer.X, buffer.Y);
+                        buffer =  null;
+                        dragged = true;
+                    }
+                }
 
-                self.eventManager.handleCanvasEvent(new GEvent(GEvent.CNVS_DRG, "", []));
+                if(buffer == null){
+                    self.viewTransform.addX(diff.X);
+                    self.viewTransform.addY(diff.Y);
+                }
+
+                self.eventManager.handleCanvasEvent(new GEvent(GEvent.CNVS_DRG, "", [])); // In case there are special drag effects
             }
         });
 

0508006340f9aab51f81f2a36e1545369f96c96d
Calvin McMurray <calvin.r.mcm@gmail.com>
2016-07-05 11:34:41 -0600

    Made up the Main Menu Interface loading function in the PanelManager and organized the constant types in the GEvent.

--- src/js/GEvent.js
+++ src/js/GEvent.js
@@ -17,7 +17,9 @@ define([],function(){
         this.data = data; // An array of data
     }
 
+    // INTERFACES ==============================================================================[]>
     GEvent.LD_INTFC = "load_interface";     // Signal for loading an interface
+    GEvent.INTFC_LD = "interface_loaded";   // Signal for interface loaded
 
     GEvent.SP_INTFC = "splash_interface";   // The standard splash interface
     GEvent.MM_INTFC = "main_menu_interface";// The main menu interface
@@ -25,40 +27,44 @@ define([],function(){
     GEvent.LV_INTFC = "level_interface";    // The standard level selecting interface
     GEvent.UG_INTFC = "upgrade_interface";  // The standard upgrade management interface
 
-    GEvent.LD_SDBAR = "load_sidebar";       // Signal for sidebar needing loaded
+    // TOP BARS ================================================================================[]>
     GEvent.LD_TPBAR = "load_topbar";        // Signal for topbar needing loaded
-
-    GEvent.INTFC_LD = "interface_loaded";   // Signal for interface loaded
-    GEvent.SDBAR_LD = "sidebar_loaded";     // Signal for sidebar HTML loaded
     GEvent.TPBAR_LD = "topbar_loaded";      // Signal for topbar HTML loaded
-
-
-    GEvent.BLDG_PNL = "building_panel";     // Building Panel
-    GEvent.INDX_PNL = "indexer_panel";      // Indexer Panel
-
-    GEvent.BLNK_PNL = "blank_panel";        // Load a blank panel
 
     GEvent.GM_TPBAR = "game_top_bar";       // Game Top Bar
 
+    // SIDE BARS ===============================================================================[]>
+    GEvent.LD_SDBAR = "load_sidebar";       // Signal for sidebar needing loaded
+    GEvent.SDBAR_LD = "sidebar_loaded";     // Signal for sidebar HTML loaded
+
+    GEvent.BLDG_PNL = "building_panel";     // Building Panel
+    GEvent.INDX_PNL = "indexer_panel";      // Indexer Panel
+    GEvent.BLNK_PNL = "blank_panel";        // Load a blank panel
+
     GEvent.ST_CLICK = "set_click";          // Signal what should happen on a click event.
+
+    // CANVAS SKINS ============================================================================[]>
+    GEvent.VTRY_PNL = "victory_panel";      // Victory Panel
+    GEvent.DEFT_PNL = "defeat_panel";       // Defeat Panel
+
+    // CLICKING ================================================================================[]>
+    GEvent.CNVS_CLK = "canvas_click";       // The canvas click event type
+    GEvent.CNVS_DRG = "canvas_drag";        // The canvas drag event type
+
+
+    // BUTTON SIGNALS ==========================================================================[]>
+    GEvent.BTN_ACTN = "button_action";      // Button Action
+
+    GEvent.NEXT_BTN = "next_level_button";  // Next Level Button
+    GEvent.AGAN_BTN = "play_again_button";  // Play Level Again Button
+    GEvent.MENU_BTN = "menu_button";        // Menu Button
 
     GEvent.STAN_BLD = "standard_building";  // The standard type of building
     GEvent.LIBR_BLD = "library_building";   // The library type of building
 
     GEvent.STAN_IDX = "standard_indexer";   // The standard type of indexer
     GEvent.HOBB_IDX = "hobbyist_indexer";   // The hobbyist type of indexer
-    GEvent.UBER_IDX = "uber_indexer";       // The hobbyist type of indexer
-
-    GEvent.CNVS_CLK = "canvas_click";       // The canvas click event type
-    GEvent.CNVS_DRG = "canvas_drag";        // The canvas drag event type
-
-    GEvent.VTRY_PNL = "victory_panel";      // Victory Panel
-    GEvent.DEFT_PNL = "defeat_panel";       // Defeat Panel
-    GEvent.NEXT_BTN = "next_level_button";  // Next Level Button
-    GEvent.AGAN_BTN = "play_again_button";  // Play Level Again Button
-    GEvent.MENU_BTN = "menu_button";        // Menu Button
-
-    GEvent.BTN_ACTN = "button_action";      // Button Action
+    GEvent.UBER_IDX = "uber_indexer";       // The unbeatable type of indexer
 
     return GEvent;
 });

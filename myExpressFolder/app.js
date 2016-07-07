5e88348bcbf6fb5e3c6abd14cb5962a64067a98f
Calvin McMurray <calvin.r.mcm@gmail.com>
2016-07-06 18:13:01 -0600

    Configured mongodb andset the server to listen on 8080.

--- myExpressFolder/app.js
+++ myExpressFolder/app.js
@@ -23,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.set('port', 8080);
+app.listen(app.get('port'));
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
--- myExpressFolder/routes/index.js
+++ myExpressFolder/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.sendFile('index.html', {root:public});
+});
+
+mongoose.connect("mongodb://localhost/persons");
+
+var personSchema = mongoose.Schema({
+    pid: String,
+    data: Object
+});
+
+var Person = mongoose.model('Person', personSchema); // Make template object.
+
+var db = mongoose.connection;
+
+var myguy = new Person({pid: "stuff", data: {a:'a', b:'b'}});
+
+myguy.save();
+
+db.once('open', function(){
+    console.log("DB OPEN");
 });
 
 module.exports = router;

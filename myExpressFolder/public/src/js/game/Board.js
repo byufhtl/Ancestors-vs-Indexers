55c7100a2bd43ad5906f71d22595ccabeab9093a
Zachary Nelson <zach.t.nelson01@gmail.com>
2016-08-25 16:00:53 -0600

    stubbed out level definition and created/stubbed Board class

---
+++ myExpressFolder/public/src/js/game/Board.js
@@ -0,0 +1,31 @@
+define([],function() {
+
+    function Board(){}
+
+    //generates and returns the 2d array representing all board tiles
+    Board.generate = function(levelData){
+
+    };
+
+    //
+    Board.makeCycles = function(){
+
+    };
+
+    //
+    Board.combineCycles = function(){
+
+    };
+
+    //
+    Board.makeClumpes = function(){
+
+    };
+
+    //
+    Board.combineClumps = function(){
+
+    };
+
+    return Board;
+});

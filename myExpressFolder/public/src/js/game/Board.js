4802ccd393a4c6e9ffced9a71dad2738e37e2a4f
CalvinMcMurray <calvin.mcmurray@byu.edu>
2016-08-25 16:10:23 -0600

    Made some better comments on our stub class.

--- myExpressFolder/public/src/js/game/Board.js
+++ myExpressFolder/public/src/js/game/Board.js
@@ -2,27 +2,44 @@ define([],function() {
 
     function Board(){}
 
-    //generates and returns the 2d array representing all board tiles
+    /**
+     * Generates and returns the 2d array representing all board tiles
+     * @param levelData {#ofDatabases, #extraClumps, #Clumpiness}
+     */
     Board.generate = function(levelData){
-
+        /*
+            For # of clumps
+                var clump = makeClump();
+                Add it to the field
+            Combine clumps
+        */
     };
 
-    //
+    /**
+     * Makes a cycle.
+     */
     Board.makeCycles = function(){
 
     };
 
-    //
+    /**
+     * Combines cycles within a clump.
+     */
     Board.combineCycles = function(){
 
     };
 
-    //
-    Board.makeClumpes = function(){
+    /**
+     * Creates a single clump, represented as a two-dimensional array of Tiles. Spaces with no tiles should be
+     * represented by null.
+     */
+    Board.makeClump = function(){
 
     };
 
-    //
+    /**
+     * Builds the links between clumps within the field.
+     */
     Board.combineClumps = function(){
 
     };
